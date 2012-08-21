3c2029afd42bbf0e8d536b4e63eaf878340a0a50
Scott Dover <sdover102@me.com>
2012-08-21 13:59:13 -0400

    Made a tweak to CSS / Config

--- app/config.php
+++ app/config.php
@@ -1,4 +1,4 @@
 <?php
-define('DOCUMENT_TITLE', 'Title of the document');
+define('DOCUMENT_TITLE', 'Markdown Doc');
 define('PASSWORD', 'Title of the document');
 define('USE_PASSWORD_FOR_ACCESS', 'Title of the document');
