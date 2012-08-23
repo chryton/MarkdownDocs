d536c948e56d6411c399b4998f03ad96f93943bc
Scott Dover <sdover102@me.com>
2012-08-23 07:01:46 -0400

    Made a small patch to web root path

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ define('APP_DIR', ROOT_DIR.'app/');
 
 /* Make sure web_root doesn't have trailing slash */
 $web_root = str_replace($_SERVER['DOCUMENT_ROOT'], '', ROOT_DIR);
+if ($web_root[0] != '/') $web_root = '/' . $web_root;
 $web_root = $web_root[strlen($web_root) - 1] == '/' ? substr($web_root, 0, strlen($web_root) - 1) : $web_root;
 define('WEB_ROOT', $web_root);
 
