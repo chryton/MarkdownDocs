1659eae2c356deaff721d3431a952d260ad29e93
Scott Dover <sdover102@me.com>
2012-08-23 06:57:17 -0400

    Changed default DOCUMENT_TITLE

--- app/config.php
+++ app/config.php
@@ -1,5 +1,5 @@
 <?php
 
-define('DOCUMENT_TITLE', 'Everwondr Programming Notes');
+define('DOCUMENT_TITLE', 'MarkdownDocs');
 define('PASSWORD', 'password');
 define('USE_PASSWORD_FOR_ACCESS', FALSE);
