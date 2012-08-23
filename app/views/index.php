ec6b7c6db53dfc28357ba5310c4dd5ae7656f4d7
Scott Dover <sdover102@me.com>
2012-08-22 21:17:43 -0400

    Updated the footer to reflect new Github link

--- app/views/index.php
+++ app/views/index.php
@@ -3,6 +3,6 @@
 		<?php echo $page->content; ?>
 	</div> <!-- /content -->
 	<div class="footer">
-		Get this on <a href="https://github.com/sdover102/Doc_viewer" target="_blank">Github</a>
+		Get this on <a href="https://github.com/sdover102/MarkdownDocs" target="_blank">Github</a>
 	</div> <!-- /footer -->
 </div> <!-- /tpl-content -->
